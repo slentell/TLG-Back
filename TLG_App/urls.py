04112a0cb9d0577658d593a1253d9ea2186b6915
K-Belk <kevinlbelk229@gmail.com>
2022-06-30 18:33:48 -0500

    added bell ringer by team

--- TLG_App/urls.py
+++ TLG_App/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from rest_framework import routers
-from .views import ImageGalleryViewSet, TeamViewSet, AthleteViewSet, LiftHistoryViewSet, MaxLiftByTeamViewSet, PostsViewSet, AthleteByTeamViewSet
+from .views import ImageGalleryViewSet, TeamViewSet, AthleteViewSet, LiftHistoryViewSet, MaxLiftByTeamViewSet, PostsViewSet, AthleteByTeamViewSet, DevinStoleMyShitViewSet
 
 router = routers.DefaultRouter()
 router.register(r'team', TeamViewSet)
@@ -8,8 +8,10 @@ router.register(r'athlete', AthleteViewSet)
 router.register(r'lift-history', LiftHistoryViewSet)
 router.register(r'max-lift-by-team', MaxLiftByTeamViewSet, basename='max-lift-by-team')
 router.register(r'posts', PostsViewSet)
-router.register(r'image-gallery', ImageGalleryViewSet)
-router.register(r'athlete-by-team', AthleteByTeamViewSet, basename='athlete-by-team' )
+router.register(r'image-gallery', ImageGalleryViewSet),
+router.register(r'athlete-by-team', AthleteByTeamViewSet, 
+basename='athlete-by-team' )
+router.register(r'bell-ringer-by-team', DevinStoleMyShitViewSet, basename='bell-ringer-by-team'), 
 
 
 
